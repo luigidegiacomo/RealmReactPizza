16e00dd94d3de58347d1ce34df71492f6a31625c
luigi <luigi.degiacomo@gmail.com>
2021-10-12 20:21:03 +0200

    tech

---
+++ RealmReactPizza/Controllers/OrdinazioniController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PizzApp.Models;
+using PizzApp.Services;
+
+namespace RealmReactPizza.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrdinazioniController : ControllerBase
+    {
+
+        private readonly ILogger<OrdinazioniController> _logger;
+
+        public OrdinazioniController(ILogger<OrdinazioniController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<RigaOrdine>> Get()
+        {
+            string partition = "02";
+            var listaOrdinazioni = await RealmDataStore.ListaOrdiniDaEvadere(partition);
+            return listaOrdinazioni;
+        }
+    }
+}
